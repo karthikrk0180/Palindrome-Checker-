ab43fa0690c19e47067b974f6b0f2c42bf8bb63a
karthikrk0180 <karthikrk0180@gmail.com>
2024-04-09 22:35:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const input = document.getElementById("input");
+
+function reverseString(str) {
+    return str.split("").reverse().join("")
+}
+
+function check(){
+    const value = input.value;
+    const reverse = reverseString(value);
+    
+
+    if (value === reverse){
+        alert("Palindrome");
+    }
+    else{
+        alert("Not Palindrome");
+    }
+
+    input.value = "";
+}
